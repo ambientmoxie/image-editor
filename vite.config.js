daf53fd72f426e893377a566972fcd5edc632a95
ambientmoxie <contact@maximebenoit.work>
2024-10-21 14:51:53 +0200

    edited: vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/projects/img-editor",
+  base: "/tool/img-editor",
   server: { host: true },
 });
